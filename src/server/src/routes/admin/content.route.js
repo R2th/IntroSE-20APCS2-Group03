1ce9d3eb67d011c6cbceb70a11faba1a9f810a43
ngxquang <nq23092016@gmail.com>
2022-12-03 11:40:36 +0700

    Fix some filenames

--- src/server/src/routes/admin/content.route.js
+++ src/server/src/routes/admin/content.route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
-const upload = require("../../controllers/upload");
+const upload = require("../../controllers/upload.controller");
 
 router.post(
   "/admin/upload-image",
