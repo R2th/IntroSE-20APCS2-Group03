9ba794461ec34a2d8f8614fe5edd92e72ad80400
ngxquang <nq23092016@gmail.com>
2022-12-03 10:30:22 +0700

    Database sync

--- src/server/index.js
+++ src/server/index.js
@@ -7,6 +7,28 @@ const port = process.env.PORT || 2022;
 const DEFAULT_PORT = 3000;
 const HOST = "localhost";
 
+const db = require("./src/db/index")
+const Role = db.role;
+db.sequelize.sync({force: true})
+.then(() =>{
+  console.log("Sync database");
+  initiate()
+})
+.catch((err) =>{
+  console.log(err);
+})
+
+const initiate = async () =>{
+  await Role.create({
+    id: 1,
+    roleName: "user"
+  });
+  await Role.create({
+    id: 2,
+    roleName: "admin"
+  });
+}
+
 http.listen(port, () => {
   const { 2: mode } = process.argv;
   if (mode)
