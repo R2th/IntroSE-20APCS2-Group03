5e0aaef065a5dd034f422a38ba288140e3403d0d
ngxquang <nq23092016@gmail.com>
2022-12-05 00:20:28 +0700

    Setup Docker for database

---
+++ src/server/db/init.sql
@@ -0,0 +1,7 @@
+CREATE USER bytesgo;
+
+CREATE DATABASE bytesgodb_dev;
+GRANT ALL PRIVILEGES ON DATABASE bytesgodb_dev TO bytesgo;
+
+CREATE DATABASE bytesgodb_test;
+GRANT ALL PRIVILEGES ON DATABASE bytesgodb_test TO bytesgo;
