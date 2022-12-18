d23faef312bc1e8df958cc0ffc6895c36a7765e4
ngxquang <nq23092016@gmail.com>
2022-12-18 16:15:05 +0700

    define routers for story

--- src/server/src/routes/story/story.route.js
+++ src/server/src/routes/story/story.route.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const verifyToken = require("../../middleware/isLogin");
+const isAdmin = require("../../middleware/isAdmin");
 const storyController = require("../../controllers/story.controller");
 const { upload } = require("../../controllers/upload.controller");
 
-// Get story
-// router.get("/story/:storyId/", storyController.getStory);
 
-// router.get("/story/:storyId/update", storyController.updateStory);
 
 // Crawl the story
 
 router.post("/story/crawl", upload.single("story"), storyController.crawlStory);
+router.put("/story/:storyId", verifyToken, storyController.updateStory);
+router.delete("/story/", verifyToken, storyController.deleteStory);
+router.get("/story/:storyId", storyController.getStoryByStoryId);
+router.post("/story/", verifyToken, storyController.createStory);
+router.post("/story/views", verifyToken, storyController.updateStoryView);
+router.post("/story/vote/", verifyToken, storyController.voteStory);
 
 module.exports = router;
